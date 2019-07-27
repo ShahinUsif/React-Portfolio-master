8030d3e774a48448cacefdaa2fe20b44a6ed510d
Yousef Shahin <usif.shahin@gmail.com>
2019-07-27 21:31:30 +0300

    project init

---
+++ src/resumeData.js
@@ -0,0 +1,150 @@
+let resumeData = {
+  "imagebaseurl": "https://rbhatia46.github.io/",
+  "name": "Fatima Bahry Yasien",
+  "role": "Software Engineer",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": " I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://twitter.com/",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "I am currently a final year student at Philadelphia University of Information Technology and pursuing my career from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address": "Jordan",
+  "website": "https://www.google.com",
+  "education": [
+    {
+      "UniversityName": "Applied Science Private University",
+      "specialization": "Software Engineering",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2012",
+      "Achievements": "Bachelor Degree of Information Technology"
+    },
+    {
+      "UniversityName": "Philadelphia University",
+      "specialization": "Software Engineering",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2013",
+      "Achievements": "Bachelor Degree of Information Technology"
+    },
+    {
+      "UniversityName": "Philadelphia University",
+      "specialization": "MIS",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2018",
+      "Achievements": "Bachelor Degree of Information Technology"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Falcons Soft",
+      "specialization": "Software Developer",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2019",
+      "Achievements": "Working on developing ERP Systems"
+    },
+    {
+      "CompanyName": "Mena Itech",
+      "specialization": "Software Developer",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Training on Developing Web Cloud Solution"
+    },
+    {
+      "CompanyName": "MBC",
+      "specialization": "Software Developer",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2017",
+      "Achievements": "Training on Mobile Application Development"
+    }
+  ],
+  "skillsDescription": "Technical Skills",
+  "skills": [
+    {
+      "skillname": "ReactJS"
+    },
+    {
+      "skillname": "NodeJS"
+    },
+    {
+      "skillname": "Javascript"
+    },
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS3"
+    },
+    {
+      "skillname": "Java"
+    },
+    {
+      "skillname": "php"
+    },
+    {
+      "skillname": "ASP"
+    },
+    {
+      "skillname": "OracleDB"
+    }
+    ,
+    {
+      "skillname": "PostgreSql"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "Accounting Management System",
+      "description": "webapp",
+      "imgurl": "images/portfolio/accounting.jpg"
+    },
+    {
+      "name": "Restaurant Mobile POS",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/coffee-shop.jpg"
+    },
+    {
+      "name": "Pharmacy Management Application",
+      "description": "webapp",
+      "imgurl": "images/portfolio/pharmacy.jpg"
+    },
+    { 
+      "name": "Payroll Application",
+      "description": "webapp",
+      "imgurl": "images/portfolio/payroll.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "A Certified Ethical Hacker is a skilled professional who understands and knows how to look for weaknesses and vulnerabilities in target systems and uses the same knowledge and tools as a malicious hacker, but in a lawful and legitimate manner to assess the security posture of a target system(s).",
+      "name": "Certified Ethical Hacker"
+    },
+    {
+      "description": "Learned how to efficiently develop and deploy exciting applications and services, using Java SE, Java EE, and Java embedded technologies.",
+      "name": "J2SE & J2EE Certificate"
+    },
+    {
+      "description": "The .NET Programming Certificate provides students with an opportunity to learn .NET application development. It targets students who want to acquire skills needed for .NET programming, web programming, and web site development. It also covers ASP.NET, a technology for creating Web applications. The Certificate's database management course utilizes Microsoft SQL Server.",
+      "name": "ASP .NET Programming Certificate"
+    },
+    {
+      "description": "a standardized test administered by the Educational Testing Service (ETS) to create a baseline for comparison of students by their English language capabilities. The TOEFL exam is comprised of 4 sections: reading, listening, speaking, and writing.",
+      "name": "TOEFL Certificate"
+    }
+  ]
+}
+
+export default resumeData
